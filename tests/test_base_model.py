962ecbe7f2bdb2e581dbaf7eac595aa602ff57fe
peluza <edisonisaza@gmail.com>
2020-06-25 11:47:11 -0500

    test_base_model.py

---
+++ tests/test_base_model.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""test_base_model
+"""
+import unittest
+import pep8
+from models/base_model import BaseModel
+
+
+class TestBase(unittest.TestCase):
+    """TextMaxInteger(unittest.TesCase)
+
+    Args:
+        unittest (Test): analysis of data
+    """
+
+    def test_pep8_conformance_base(self):
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/base_model.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
