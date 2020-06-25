5112f4a1b9edc1becc0ca9cebda9cdce69d1900b
peluza <edisonisaza@gmail.com>
2020-06-25 16:55:51 -0500

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -8,10 +8,17 @@ from datetime import datetime
 
 class BaseModel:
 
-    def __init__(self, id=0, created_at=0, updated_at=0):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
+    def __init__(self, *args, **kwargs):
+        if kwargs is not None and kwargs != {}:
+            self.id = kwargs['id']
+            self.created_at = datetime.strptime(
+                kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            self.updated_at = datetime.strptime(
+                kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.today()
+            self.updated_at = datetime.today()
 
     def __str__(self):
         return str("[{}] ({}) {}".format(
@@ -26,3 +33,27 @@ class BaseModel:
         dic['created_at'] = datetime.isoformat(self.created_at)
         dic['__class__'] = self.__class__.__name__
         return dic
+
+
+# my_model = BaseModel()
+# my_model.name = "Holberton"
+# my_model.my_number = 89
+# print(my_model.id)
+# print(my_model)
+# print(type(my_model.created_at))
+# print("--")
+# my_model_json = my_model.to_dict()
+# print(my_model_json)
+# print("JSON of my_model:")
+# for key in my_model_json.keys():
+#     print("\t{}: ({}) - {}".format(key,
+#                                    type(my_model_json[key]), my_model_json[key]))
+
+# print("--")
+# my_new_model = BaseModel(**my_model_json)
+# print(my_new_model.id)
+# print(my_new_model)
+# print(type(my_new_model.created_at))
+
+# print("--")
+# print(my_model is my_new_model)
