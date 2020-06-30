275db882451ac2695c57d4582775d9dc154a004e
peluza <edisonisaza@gmail.com>
2020-06-30 05:51:02 +0000

    add commit the class

--- models/city.py
+++ models/city.py
@@ -4,6 +4,11 @@ from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """ class city """
+    """city
+
+    Args:
+        BaseModel (class):  the class is father
+
+    """
     state_id = ""
     name = ""
