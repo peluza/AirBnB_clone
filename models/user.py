b0e6a8dea4302ede5d8cc9de8548e0a04c21e82b
peluza <edisonisaza@gmail.com>
2020-06-30 05:51:45 +0000

    add commit the class

--- models/user.py
+++ models/user.py
@@ -4,6 +4,11 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
+    """User
+
+    Args:
+        BaseModel (class):  the class is father
+    """
     email = ""
     password = ""
     first_name = ""
