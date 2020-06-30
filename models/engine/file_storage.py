fdfcbd48657d1bb5f2eded95a8bce6f8953cda56
peluza <edisonisaza@gmail.com>
2020-06-30 05:41:28 +0000

    add commit the class and funtion

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,17 +9,30 @@ from models.user import User
 
 
 class FileStorage:
+    """ class FileStorage """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
+        """all
+
+        Returns:
+            [dic]: variable privade __objetcs
+        """
         return self.__objects
 
     def new(self, obj):
+        """new
+
+        Args:
+            obj (dic): the objects is diccionary
+        """
         key = obj.__class__.__name__ + "." + obj.id
         self.__objects[key] = obj
 
     def save(self):
+        """save elements in the file.json
+        """
         mydict = {}
         for k, v in self.__objects.items():
             mydict[k] = v.to_dict()
@@ -27,6 +40,8 @@ class FileStorage:
             json.dump(mydict, my_file)
 
     def reload(self):
+        """reload elements in the file.json
+        """
         if os.path.exists(self.__file_path):
             with open(self.__file_path, 'r', encoding='utf-8') as my_file:
                 str_read = my_file.read()
